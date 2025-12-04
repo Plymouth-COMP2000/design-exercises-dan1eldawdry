415f1f82b30ef9fc29c4e05e30e54412cc57f9df
dan1eldawdry <daniel.dawdry@students.plymouth.ac.uk>
2025-12-04 13:31:04 +0000

    Added Manage Menu UI and Navigation functionality

--- app/src/main/java/com/example/restaurantguestapp/ManageMenuActivity.java
+++ app/src/main/java/com/example/restaurantguestapp/ManageMenuActivity.java
@@ -1,24 +1,62 @@
 package com.example.restaurantguestapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class ManageMenuActivity extends AppCompatActivity {
+
+    private Button logoutButton;
+    private Button addNewItemButton;
+    private Button dashboardNavButton;
+    private Button manageMenuNavButton;
+    private Button viewReservationsNavButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_manage_menu);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        logoutButton = findViewById(R.id.button_logout);
+        addNewItemButton = findViewById(R.id.button_add_new_menu_item);
+        dashboardNavButton = findViewById(R.id.button_nav_dashboard);
+        manageMenuNavButton = findViewById(R.id.button_nav_manage_menu);
+        viewReservationsNavButton = findViewById(R.id.button_nav_view_reservations);
+
+        setupButtonListeners();
+    }
+
+    private void setupButtonListeners() {
+
+        logoutButton.setOnClickListener(v -> logoutUser());
+
+        addNewItemButton.setOnClickListener(v -> {
+            // will add this later on
         });
+
+        dashboardNavButton.setOnClickListener(v -> {
+            Intent intent = new Intent(ManageMenuActivity.this, StaffDashboardActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+        manageMenuNavButton.setOnClickListener(v -> {
+        });
+
+        viewReservationsNavButton.setOnClickListener(v -> {
+            Intent intent = new Intent(ManageMenuActivity.this, ViewReservationsActivity.class);
+            startActivity(intent);
+            finish();
+        });
+    }
+
+    private void logoutUser() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 }
