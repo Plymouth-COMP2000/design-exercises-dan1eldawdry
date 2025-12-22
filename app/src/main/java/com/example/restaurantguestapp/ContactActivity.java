969819ee0b4357654edbdcd06b14954ed408df65
dan1eldawdry <daniel.dawdry@students.plymouth.ac.uk>
2025-12-22 15:37:00 +0000

    tidied contact screen

--- app/src/main/java/com/example/restaurantguestapp/ContactActivity.java
+++ app/src/main/java/com/example/restaurantguestapp/ContactActivity.java
@@ -8,11 +8,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class ContactActivity extends AppCompatActivity {
 
+    // bottom navigation buttons
     private Button menuButton;
     private Button reservationsButton;
-    private Button callButton;
     private Button logoutButton;
-    String username;
+
+    private String username; // stores current users username
 
 
     @Override
@@ -22,17 +23,18 @@ public class ContactActivity extends AppCompatActivity {
 
         username = getIntent().getStringExtra("username"); // gets username that was passed through
 
-        // bottom navigation buttons
-        menuButton = findViewById(R.id.button_menu);
-        reservationsButton = findViewById(R.id.button_reservations);
-        callButton = findViewById(R.id.button_call);
-        logoutButton = findViewById(R.id.button_logout);
+        // link ui and java
+        menuButton = findViewById(R.id.menuButton);
+        reservationsButton = findViewById(R.id.reservationsButton);
+        logoutButton = findViewById(R.id.logoutButton);
 
-        setupButtonListeners();
+        setupButtons();
     }
 
-    private void setupButtonListeners() {
-        // listener for menu
+    // click listeners for all buttons on screen
+    private void setupButtons() {
+
+        // open menu screen
         menuButton.setOnClickListener(v -> {
             Intent intent = new Intent(this, MenuListActivity.class);
             intent.putExtra("username", username);
@@ -40,25 +42,21 @@ public class ContactActivity extends AppCompatActivity {
 
         });
 
-        // listener for reservation
+        // open reservation screen
         reservationsButton.setOnClickListener(v -> {
             Intent intent = new Intent(this, MyReservationsActivity.class);
             intent.putExtra("username", username);
             startActivity(intent);
         });
 
-        // listener for call
-        callButton.setOnClickListener(v -> {
-        });
-
+        // log user out
         logoutButton.setOnClickListener(v -> logoutUser());
     }
 
-    // logout method
+    // logs user out and returns them to login screen
     private void logoutUser() {
         Intent intent = new Intent(this, LoginActivity.class);
-        // clear previous activites and start new screen
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // clears activity stack so cannot return
         startActivity(intent);
         finish();
     }
